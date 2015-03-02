97519c4445e7e917abe8cc1d8647e679a799489c
frequ <samppa.valkama@gmail.com>
2015-03-02 14:00:34 +0200

    yt-embedder somewhat fixed, can't call play on mobile devices programmatically

--- app/js/app.js
+++ app/js/app.js
@@ -13,12 +13,13 @@
         });
     })
 
-    .controller('MainCtrl', function($scope, $location, $log, $window){
+    .controller('MainCtrl', function($scope, $location, $log, $timeout, $document){
 
+        $scope.oncePlayed = false;
         $scope.playing = false;
         $scope.message = 'O-ou. Videon id puuttuu. Lisää osoitteen perään ?videoId=videonId.';
         $scope.playerVars = {
-            controls: 0,
+            //controls: 0,
             rel: 0,
             showinfo: 0,
             iv_load_policy: 3,
@@ -26,15 +27,22 @@
             cc_load_policy: 0
         };
         $scope.$on('youtube.player.ready', function($event, player){
-            player.playVideo();
+            $log.info('player ready');
+            $scope.message = "Player ready!";
+
+            //player.playVideo(); this doesnt work on the first time inited in android tab
+
         });
 
         $scope.$on('youtube.player.playing', function($event, player){
+            $scope.message = "Playing!";
             $log.info('yt playing');
             $scope.playing = true;
+            $scope.oncePlayed = true;
         });
 
         $scope.$on('youtube.player.paused', function($event, player){
+            $scope.message = 'Paused!';
             $log.info('yt paused');
             $scope.playing = false;
         });
@@ -42,10 +50,6 @@
 
         $scope.getParams = function(){
             $scope.videoId = $location.search().videoId;
-        };
-
-        $scope.historyBack = function(){
-            $window.history.back();
         };
 
         $scope.$on('youtube.player.ended', function ($event, player) {
